780bbd681577135e079b4fd9c7ead35f6b27c692
Alexander Kostin <asurv2@gmail.com>
2022-10-20 12:16:35 +0300

    refactoring

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
-}
+const image = { uri: "https://reactjs.org/logo-og.png" };
+
+const App = () => (
+    <View style={styles.container}>
+        <ImageBackground source={image} style={styles.image}>
+            <Text style={styles.text}>Inside</Text>
+        </ImageBackground>
+    </View>
+);
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
+    },
+    image: {
+        flex: 1,
         justifyContent: "center",
+        resizeMode: "cover",
+        // width: 700,
+        height: 700,
+    },
+    text: {
+        color: "white",
+        fontSize: 42,
+        lineHeight: 84,
+        fontWeight: "bold",
+        textAlign: "center",
+        backgroundColor: "#000000c0",
     },
 });
+
+export default App;
